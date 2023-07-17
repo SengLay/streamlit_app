6a7c1720f9e66a8f71994ae3e2249b63f82c1be6
Seng Lay <99320774+SengLay@users.noreply.github.com>
2023-07-17 23:38:55 +0700

    Update tst.py

--- tst.py
+++ tst.py
@@ -1,37 +1,37 @@
-# import streamlit as st
-# import os
+import streamlit as st
+import os
 
 
-# def load_data(file_path):
-#     # Use os.path.join to create the file path
-#     data_path = os.path.join(os.getcwd(), file_path)
+def load_data(file_path):
+    # Use os.path.join to create the file path
+    data_path = os.path.join(os.getcwd(), file_path)
 
-#     # Check if the file exists
-#     if os.path.exists(data_path):
-#         # Load the data
-#         data = data_path
-#         return data
-#     else:
-#         st.error("File not found: {}".format(data_path))
+    # Check if the file exists
+    if os.path.exists(data_path):
+        # Load the data
+        data = data_path
+        return data
+    else:
+        st.error("File not found: {}".format(data_path))
 
 
-# def main():
-#     st.title("My Streamlit App")
+def main():
+    st.title("My Streamlit App")
 
-#     # Assume the data file is located in the same directory as the app
-#     data_file = "Photo/moey.png"
-#     data_file_2 = "Photo/itc.png"
+    # Assume the data file is located in the same directory as the app
+    data_file = "Photo/moey.png"
+    data_file_2 = "Photo/itc.png"
 
-#     # Load the data using the relative file path
-#     data = load_data(data_file)
-#     data_2 = load_data(data_file_2)
+    # Load the data using the relative file path
+    data = load_data(data_file)
+    data_2 = load_data(data_file_2)
 
-#     # Display the data in the app
-#     st.write(data)
-#     st.image(data)
-#     st.image(data_2)
+    # Display the data in the app
+    st.write(data)
+    st.image(data)
+    st.image(data_2)
     
 
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
