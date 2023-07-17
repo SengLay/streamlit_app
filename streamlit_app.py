97df5ae94479ac47af15aea239b8fdc5c5aac431
Seng Lay <99320774+SengLay@users.noreply.github.com>
2023-07-17 23:05:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1260,11 +1260,10 @@ def EDA_Salary():
 EDA_Salary()
 
 # Show pdf: report and guideline
-def show_pdf(file_path):
-    with open(file_path, "rb") as f:
+def show(pdf_file):
+    with open(pdf_file, "rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-
-    pdf_display = f'<center><embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="800" type="application/pdf"></embed></center>'
+    pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
     st.markdown(pdf_display, unsafe_allow_html=True)
 
 st.markdown("""<div id="report"></div>""", unsafe_allow_html=True)
