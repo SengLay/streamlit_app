547e7a28a7b94f615f65203bbb3cc78bf2620591
Seng Lay <99320774+SengLay@users.noreply.github.com>
2023-07-17 23:14:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1279,9 +1279,11 @@ def get_pdf_download_link(file_path):
     return download_link
 
 st.markdown("""<div id="report"></div>""", unsafe_allow_html=True)
+st.write("👇 Job Analysis Report | Click the link down below 📑")
 # st.markdown("<div style='font-size: 30px';><center><b>Job Analysis Report</b></center></div>", unsafe_allow_html=True)
 show_pdf("PDF Report/Report.pdf")
 
 st.markdown("""<div id="guideline"></div>""", unsafe_allow_html=True)
+st.write("👇 Job Analysis Guideline | Click the link down below 📑")
 # st.markdown("<div style='font-size: 30px';><center><b>Job Analysis Guideline</b></center></div>", unsafe_allow_html=True)
 show_pdf("PDF Report/Guideline.pdf")
